2997374034134a065441e046c60dbc2ece238411
plumi-cyber <pelumi.kola@icloud.com>
2026-04-04 20:46:50 -0600

    set_permissions.sh

---
+++ docs/docs/docs/scripts/set_permissions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Permission-setting script for secure cloud migration
+# Autism Today Foundation Practicum
+# Author: Oluwapelumi Kolawole
+
+# [To be populated during implementation phase]
+
+echo "Permission script placeholder"
